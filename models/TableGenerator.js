e54227c408708ca5a360869d0f5fd0308f0287d8
Daniel den Hartog <daniel.mista@gmail.com>
2018-08-15 14:43:57 +0100

    Successfully added space to DB

--- models/TableGenerator.js
+++ models/TableGenerator.js
@@ -8,11 +8,7 @@ const clientTest = new pg.Client(dbTest)
 clientTest.connect()
 clientTest.query('CREATE TABLE listings(id SERIAL PRIMARY KEY, title VARCHAR(60), listing_desc VARCHAR(200), owner VARCHAR(60), price NUMERIC(6,2), Address VARCHAR(200));', (err, res) => {
   console.log(err, res)
-  clientTest.end()
 })
-
-clientDev.connect()
 clientDev.query('CREATE TABLE listings(id SERIAL PRIMARY KEY, title VARCHAR(60), listing_desc VARCHAR(200), owner VARCHAR(60), price NUMERIC(6,2), Address VARCHAR(200));', (err, res) => {
   console.log(err, res)
-  clientDev.end()
 })
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
+const bodyParser = require('body-parser');
 var ejs = require('ejs');
 var path = require('path');
+const pg = require('pg')
+const dbDev = require('./config')['development']['dbConnectionString']
+const clientDev = new pg.Client(dbDev)
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/', function(req, res) {
     res.render(path.join(__dirname + '/views/index.ejs'));
 });
@@ -17,7 +22,6 @@ app.get('/all', function(req, res) {
 
 app.get('/view-space', function(req, res) {
     res.render(path.join(__dirname + '/views/viewSpace.ejs'));
-    space.viewSpaces();
 });
 
 app.get('/booking-confirmation', function(req, res) {
@@ -27,6 +31,26 @@ app.get('/booking-confirmation', function(req, res) {
 app.get('/add-confirmation', function(req, res) {
     res.render(path.join(__dirname + '/views/addConfirmation.ejs'));
 });
+
+app.post('/add-space', function(req, res) {
+  console.log('post body', req.body);
+
+  clientDev.connect()
+
+  const sql = 'INSERT INTO listings (title, listing_desc, owner, price, address) VALUES ($1, $2, $3, $4, $5);'
+  const params = [req.body.title, req.body.listing_desc, req.body.owner, req.body.price, req.body.address];
+  clientDev.query(sql, params).then(function(result){
+    console.log('result?', result);
+    res.redirect('/add-confirmation');
+  }).catch(function(err){
+    console.log('err', err);
+    res.redirect('/add-confirmation');
+  });
+});
+
+
+
+
 
 app.listen(3000, function(){
   console.log("Server listening on port 3000");
